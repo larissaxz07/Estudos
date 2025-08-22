c8503fffe3f479245876770a34c42cf971afe224
Larissa Vieira <larissaaparecidavieira2023@gmail.com>
2025-08-22 10:18:14 -0300

    Semana16  PHP
    Atividades sobre o php, de Back End

---
+++ 2 ano/Back End/Semana 16/atividade01.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meu perfil</title>
+</head>
+<body>
+    <?php
+    $meuNome = "Larissa";
+    $sobrenome = "Aparecida";
+    
+    $minhaIdade = "17";
+
+    echo "Meu nome é $meuNome $sobrenome e eu tenhop $minhaIdade anos.";
+    ?>
+</body>
+</html>
---
+++ 2 ano/Back End/Semana 16/atividade04.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cartão de Visitas</title>
+</head>
+<body>
+<?php
+    $nome = "Rafael";
+    $sobrenome = "Secretário";
+    $cargo = "Secretaria";
+
+    echo "o profissional ". $nome $sobrenome "trabalha no setor Secretaria.
+
+
+
+
+
+?>
+
+</body>
+</html>
